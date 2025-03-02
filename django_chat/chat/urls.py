c1d2d83471a92090cb62c1f5616e054964d609b0
Nikita <niktos@icloud.com>
2025-03-02 22:47:56 +0300

    templates, consumers.py routing.py

--- django_chat/chat/urls.py
+++ django_chat/chat/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-   path("", views.home_view, name="home"),
-   path("groups/<uuid:uuid>/", views.group_chat_view, name="group")
+    path("", views.home_view, name="home"),
+    path("groups/<uuid:uuid>/", views.group_chat_view, name="group"),
 ]
